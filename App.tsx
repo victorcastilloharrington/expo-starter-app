943ea3d39c7fe39d60b8d27c408b07bc266a5e82
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2024-02-14 15:48:10 -0500

    updated styling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useFonts, Poppins_800ExtraBold, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import Animated, { Easing, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Poppins_800ExtraBold,
+    Poppins_400Regular
+  });
+
+  const colorScheme = useColorScheme();
+    const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+   const animConfig = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  // const animStyle = useAnimatedStyle(() => {
+  //   return {
+  //     opacity: withTiming(100, animConfig),
+  //   };
+  // });
+
+
   return (
-    <View style={styles.container}>
-      <Text>Control app</Text>
+    <SafeAreaProvider>
+    <SafeAreaView style={[styles.container, themeContainerStyle]}>
+      {/* <Animated.View style={[animStyle]}> */}
+        <Text style={[styles.text, themeTextStyle]}>CONTROLIFY APP</Text>
+      {/* </Animated.View> */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  lightContainer: {
+    backgroundColor: '#F8F9FA',
+  },
+  darkContainer: {
+    backgroundColor: '#212429',
+  },
+  lightThemeText: {
+    color: '#2e323c',
+  },
+  darkThemeText: {
+    color: '#edeff0',
+  },
+  text: {
+    fontSize: 40,
+    fontFamily: 'Poppins_800ExtraBold',
+  }
 });
